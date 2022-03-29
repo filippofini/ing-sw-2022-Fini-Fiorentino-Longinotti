18d3e50bcfe2244cf6181a2837a30a857c6144c7
lucal <luca.longinotti@mail.polimi.it>
2022-03-29 14:55:27 +0200

    Modified Professor class

--- src/main/java/it/polimi/ingsw/model/Professor.java
+++ src/main/java/it/polimi/ingsw/model/Professor.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.model;
 public class Professor {
     private final Disk_colour colour_prof;
 
-    private Professor(Disk_colour colour_prof) {
+    public Professor(Disk_colour colour_prof) {
         this.colour_prof = colour_prof;
     }
 
