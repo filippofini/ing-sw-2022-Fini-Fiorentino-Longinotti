03f5f03c0dd21a2d0cc250bff4c964a341d9857c
lucal <luca.longinotti@mail.polimi.it>
2022-03-29 14:17:09 +0200

    Added Professor class

---
+++ src/main/java/it/polimi/ingsw/model/Professor.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model;
+
+public class Professor {
+    private final Disk_colour colour_prof;
+
+    private Professor(Disk_colour colour_prof) {
+        this.colour_prof = colour_prof;
+    }
+
+    public int getColour_prof() {
+        return colour_prof.getTranslateColour();
+    }
+}
