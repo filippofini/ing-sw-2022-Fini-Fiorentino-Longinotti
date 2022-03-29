cc04e07c8e47825cf66fcb3999db599b9cda5668
filippofini <filippo.fini@mail.polimi.it>
2022-03-29 14:55:20 +0200

    Modified some classes: Assistance_card is now an enumeration.

---
+++ src/main/java/it/polimi/ingsw/model/Assistance_card.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.model;
+
+public enum Assistance_card {
+    TORTOISE(1,1), ELEPHANT(2,1),
+    BULLDOG(3,2),OCTOPUS(4,2),
+    LIZARD(5,3), FOX(6,3),
+    EAGLE(7,4), CAT(8,4),
+    OSTRICH(9,5), LION(10,5);
+
+    private final int value;
+    private final int mother_nature_movement;
+
+
+    Assistance_card(int value, int mother_nature_movement) {
+        this.value = value;
+        this.mother_nature_movement = mother_nature_movement;
+    }
+
+    public int getMother_nature_movement() {
+        return mother_nature_movement;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
--- src/main/java/it/polimi/ingsw/model/Deck.java
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -1,13 +1,29 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Deck {
-    private Assistence_card[] arr_cards;
+    private List<Assistance_card> cards;
 
    public Deck(){
-       arr_cards = new Assistence_card[10];
-       // DECK IS NOT INITIALIZED YET
+       cards = new ArrayList<Assistance_card>(Arrays.asList(
+               Assistance_card.TORTOISE,
+               Assistance_card.ELEPHANT,
+               Assistance_card.BULLDOG,
+               Assistance_card.OCTOPUS,
+               Assistance_card.LIZARD,
+               Assistance_card.FOX,
+               Assistance_card.EAGLE,
+               Assistance_card.CAT,
+               Assistance_card.OSTRICH,
+               Assistance_card.LION));
        // REMEMBER TO IMPLEMENT
 
+   }
+   public void remove_used_card(Assistance_card chosen_remove){
+       cards.remove(chosen_remove);
    }
 
 
