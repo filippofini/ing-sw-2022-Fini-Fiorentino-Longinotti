3b45c6226f8b2d3feec98244dc89dbb5a1d65692
KingOjama <101146656+KingOjama@users.noreply.github.com>
2022-04-05 15:25:02 +0200

    fIx GaMeStAtE

--- src/main/java/it/polimi/ingsw/model/Game_State.java
+++ src/main/java/it/polimi/ingsw/model/Game_State.java
@@ -4,7 +4,7 @@ public class Game_State {
     private int n_players;
     private Player[] players;
     private Game_table GT;
-    // TODO: turn_Control TC;
+    private Turn turn;
     private int[] teams;
     private boolean chat;
     private boolean expert_mode;
@@ -17,11 +17,11 @@ public class Game_State {
         this.names = names;
         this.wizard = wizard;
         this.expert_mode = expert_mode;
-
+        turn = new Turn();
         for(int i=0; i<n_players;i++){
             players[i] = new Player(names[i], wizard[i], Tower_colour.values()[i], i+1);
         }
-        GT = new Game_table(n_players);
+        GT = new Game_table(n_players,turn);
         if(n_players == 4){
             teams = new int[4];
             chat = true;
