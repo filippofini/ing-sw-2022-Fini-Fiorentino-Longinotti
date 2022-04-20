ab4f7e7f2e5015f953e4317628431bbc17415d25
KingOjama <101146656+KingOjama@users.noreply.github.com>
2022-04-20 15:59:27 +0200

    added method to add students to island and initialize students on clouds, changed arr_student type in clouds to int[]

--- src/main/java/it/polimi/ingsw/model/Cloud.java
+++ src/main/java/it/polimi/ingsw/model/Cloud.java
@@ -2,17 +2,17 @@ package it.polimi.ingsw.model;
 
 public class Cloud {
 
-    private Student[] arr_students;
+    private int[] arr_students;
 
     //the number of players affects the number of students on each island
     public Cloud(int numofPlayers) {
 
         if (numofPlayers == 2 || numofPlayers== 4) {
-            arr_students = new Student[3];
+            arr_students = new int[3];
         }
 
         if (numofPlayers == 3) {
-            arr_students = new Student[4];
+            arr_students = new int[4];
         }
     }
 
@@ -20,7 +20,7 @@ public class Cloud {
         //TODO: implement method to add students to the clouds
     }
 
-    public Student[] getArr_students() {
+    public int[] getArr_students() {
         return arr_students;
     }
 }
