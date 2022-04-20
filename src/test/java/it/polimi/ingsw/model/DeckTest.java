a6dee285801609f265225a025f1550ea5b2d94be
lucal <luca.longinotti@mail.polimi.it>
2022-04-20 16:39:29 +0200

    Modified DeckTest

--- src/test/java/it/polimi/ingsw/model/DeckTest.java
+++ src/test/java/it/polimi/ingsw/model/DeckTest.java
@@ -2,9 +2,20 @@ package it.polimi.ingsw.model;
 
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeckTest {
+
+    List<Assistance_card> cards1 = new ArrayList<>(Arrays.asList(
+            Assistance_card.TORTOISE,
+            Assistance_card.ELEPHANT,Assistance_card.CAT));
+
+    List<Assistance_card> cards2 = new ArrayList<>(Arrays.asList(Assistance_card.ELEPHANT,Assistance_card.CAT));
 
     @Test
     public void testCount(){
@@ -12,11 +23,11 @@ class DeckTest {
         assertEquals(10,10);
     }
 
+
     @Test
-    public void testRemove_used_card(){
-        Deck deck = new Deck();
-        deck.remove_used_card(Assistance_card.CAT);
-        assertNotSame(10,deck.count_elements());
+    void testremove_used_card() {
+        cards1.remove(Assistance_card.TORTOISE);
+        assertEquals(cards2,cards1);
     }
 
 }
