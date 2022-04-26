0a153ee0ab70965b4c135e8c21960d6ba90a3f05
filippofini <filippo.fini@mail.polimi.it>
2022-04-26 17:29:35 +0200

    Added some more javadoc.

--- src/main/java/it/polimi/ingsw/model/Professor.java
+++ src/main/java/it/polimi/ingsw/model/Professor.java
@@ -1,19 +1,24 @@
 package it.polimi.ingsw.model;
 
 /**
- * Class professor
+ * This class represent the professors.
+ * There is one professor of each colour. See more at: {@link it.polimi.ingsw.model.Disk_colour}.
  */
 public class Professor {
     private final Disk_colour colour_prof;
 
     /**
-     * Constructor of the class
-     * @param colour_prof colour of the professor
+     * Constructor of the class.
+     * @param colour_prof The colour of the professor.
      */
     public Professor(Disk_colour colour_prof) {
         this.colour_prof = colour_prof;
     }
 
+    /**
+     * This method returns the colour of the professor.
+     * @return An int representing the colour of the professor. See more at: {@link it.polimi.ingsw.model.Disk_colour}.
+     */
     public int getColour_prof() {
         return colour_prof.getTranslateColour();
     }
