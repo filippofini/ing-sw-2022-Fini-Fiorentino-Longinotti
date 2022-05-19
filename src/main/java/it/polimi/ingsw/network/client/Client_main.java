4f6b6f6e859f68b8799998e46adc056611990bea
filippofini <filippo.fini@mail.polimi.it>
2022-05-19 18:32:32 +0200

    Added Client main.

---
+++ src/main/java/it/polimi/ingsw/network/client/Client_main.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.network.client;
+
+//TODO: import javafx
+
+import it.polimi.ingsw.CLI.CLI;
+
+
+/**
+ * This class is used to start the game client-side.
+ * If the starting command has the argument "-cli", it starts the command line interface;
+ * if it has nothing, it starts the graphical interface.
+ */
+public class Client_main {
+
+    private final static String CLI_STARTER = "-cli";
+    private final static String HELP = "-help";
+
+    public static void main(String[] args) {
+        if (args.length==0){
+            //Application.launch(GUI.class, args)
+        } else if (args.length>1) {
+            System.out.println("Too many arguments, insert " + HELP + " to see all the available graphical interface options.");
+        } else {
+            if (CLI_STARTER.equals(args[0])){
+                CLI.main(args);
+            } else if (HELP.equals(args[0])) {
+                System.out.println("Insert " + CLI_STARTER + " to start the game in command line interface mode, otherwise insert nothing to start the GUI.");
+            } else {
+                System.out.println("Command not found, insert " + HELP + " to see the available graphical interface options.");
+            }
+        }
+    }
+
+}
