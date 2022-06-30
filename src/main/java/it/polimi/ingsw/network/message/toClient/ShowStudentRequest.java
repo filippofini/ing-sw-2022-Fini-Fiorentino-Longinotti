f74dad1ef644be119c1f695db404faba84b16271
LucaLonginotti <luca.longinotti@mail.polimi.it>
2022-06-30 23:15:39 +0200

    Added monk's messages.

---
+++ src/main/java/it/polimi/ingsw/network/message/toClient/ShowStudentRequest.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.network.message.toClient;
+
+import it.polimi.ingsw.model.Student;
+import it.polimi.ingsw.view.ViewInterface;
+
+public class ShowStudentRequest extends MessagesToClient{
+
+    Student[] students;
+
+    public ShowStudentRequest(Student[] students){
+        super(true);
+        this.students=students;
+    }
+
+    @Override
+    public void handleMessage(ViewInterface view) {
+        view.showStudent(students);
+    }
+
+
+    @Override
+    public String toString(){
+        return "Asking index island";
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/network/message/toServer/ShowStudentsReply.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.network.message.toServer;
+
+import it.polimi.ingsw.network.server.ClientHandlerInterface;
+import it.polimi.ingsw.network.server.ServerInterface;
+
+public class ShowStudentsReply implements MessagesToServer{
+    private final int index;
+
+    public ShowStudentsReply(int index){this.index=index;}
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void handleMessage(ServerInterface server, ClientHandlerInterface clientHandler) {
+
+        clientHandler.monkstudent(index);
+    }
+
+    @Override
+    public String toString() {
+        return "Index received";
+    }
+}
